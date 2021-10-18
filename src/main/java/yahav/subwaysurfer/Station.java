aa2a0e941e7a9b43a19dbe6f195b867b5b062124
Talia Yahav <taliayahav@Talias-MacBook-Air-2.local>
2021-10-18 18:23:55 -0400

    working on files

--- src/main/java/yahav/subwaysurfer/Station.java
+++ src/main/java/yahav/subwaysurfer/Station.java
@@ -1,5 +1,27 @@
 package yahav.subwaysurfer;
 
-public class Station {
+import com.google.gson.Gson;
 
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class Station {
+    List<Feature> features;
+
+    public static class Feature{
+        Properties properties;
+        Geometry geometry;
+
+    }
+    static class Geometry{
+        List<Double> coordinates;
+
+        public List getCoordinates(){
+            return coordinates;
+        }
+    }
 }
