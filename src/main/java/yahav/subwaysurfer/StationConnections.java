ad1c8527a77cacb30b03a6a009338155fa6c6a91
Talia Yahav <taliayahav@Talias-MacBook-Air-2.local>
2021-10-25 23:24:41 -0400

    connected stations

--- src/main/java/yahav/subwaysurfer/StationConnections.java
+++ src/main/java/yahav/subwaysurfer/StationConnections.java
@@ -1,9 +1,42 @@
 package yahav.subwaysurfer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StationConnections {
-    //given a station, get all the lines that exist in that station
-    //put all possible lines in diff variables
-    //go to line name and find all of it's connecting stops.
-    public void getStationLines(SubwayLines subwayLines, SubwayStations station){
+    public List<String> getStationLines(SubwayLines subwayLines, String station){
+        List<String> linesWithStation = new ArrayList<String>();
+        ArrayList<List<String>> lines = new ArrayList<List<String>>();
+        lines.add(subwayLines.A);
+        lines.add(subwayLines.B);
+        lines.add(subwayLines.C);
+        lines.add(subwayLines.D);
+        lines.add(subwayLines.E);
+        lines.add(subwayLines.F);
+        lines.add(subwayLines.G);
+        lines.add(subwayLines.J);
+        lines.add(subwayLines.L);
+        lines.add(subwayLines.M);
+        lines.add(subwayLines.N);
+        lines.add(subwayLines.Q);
+        lines.add(subwayLines.R);
+        lines.add(subwayLines.S);
+        lines.add(subwayLines.W);
+        lines.add(subwayLines.Z);
+        lines.add(subwayLines.sixExpress);
+        lines.add(subwayLines.sevenExpress);
+        lines.add(subwayLines.one);
+        lines.add(subwayLines.two);
+        lines.add(subwayLines.three);
+        lines.add(subwayLines.four);
+        lines.add(subwayLines.five);
+        lines.add(subwayLines.six);
+        lines.add(subwayLines.seven);
+        for(List<String> line: lines){
+            if(line.contains(station)){
+                linesWithStation.add(line.get(0));
+            }
+        }
+        return linesWithStation;
     }
 }
